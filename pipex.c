54ea045a4d4c668821aae64c3747e5aea538d885
Loic Pittet <lpittet@c2r4s2.42lausanne.ch>
2024-12-03 11:44:38 +0100

    added function to grab command absolute path

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: lpittet <lpittet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 09:36:18 by lpittet           #+#    #+#             */
-/*   Updated: 2024/12/02 14:23:41 by lpittet          ###   ########.fr       */
+/*   Updated: 2024/12/03 11:28:40 by lpittet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ char	*get_full_path(char *path, char *cmd)
 	path = ft_strjoin(path, cmd);
 	if (!path)
 		return (NULL);
+	return (path);
 }
 
 char	*find_cmd(char *cmd, char **env)
@@ -32,7 +33,7 @@ char	*find_cmd(char *cmd, char **env)
 	i = 0;
 	while (env[i])
 	{
-		if (ft_strnstr("PATH=", env[i], 5))
+		if (ft_strnstr(env[i], "PATH=", 5))
 		{
 			cmd_line = env[i];
 			break ;
@@ -40,31 +41,42 @@ char	*find_cmd(char *cmd, char **env)
 		i++;
 	}
 	cmd_line = cmd_line + 5;
-	src = ft_split(cmd_line, ":");
+	src = ft_split(cmd_line, ':');
 	i = 0;
 	while (src[i])
 	{
 		path = get_full_path(src[i], cmd);
-		if (access(path, F_OK));
+		if (!access(path, F_OK))
 			return (path);
 		i++;
 	}
+	perror("Command does not exist");
 	return (NULL);
 }
 
-void	child(int file, char *cmd, char **env)
+void	child(char *cmd, char **env)//int file,
 {
-	
+	char	**cmd_plus;
+	char	*path;
+
+	cmd_plus = ft_split(cmd, ' ');
+	path = find_cmd(cmd_plus[0], env);
+	printf("%s", path);
 }
 
-void	parent(int file, char *cmd, char **env)
-{
-	
-}
+// void	parent(int file, char *cmd, char **env)
+// {
+// 	char **cmd_plus;
+
+// 	cmd_plus = ft_split(cmd, ' ');
+// }
 
 int	main(int ac, char **av, char **env)
 {
-	if (ac != 5 || !av[1][1] || !av[2][1] || !av[3][1] || !av[4][1])
-		return (1);
-	
+	// if (ac != 5 || !av[1][1] || !av[2][1] || !av[3][1] || !av[4][1])
+	// 	return (1);
+	if (ac)
+		ft_printf("go\n");
+	child(av[1], env);
+	return (0);
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: lpittet <lpittet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 09:36:48 by lpittet           #+#    #+#             */
-/*   Updated: 2024/12/02 09:49:42 by lpittet          ###   ########.fr       */
+/*   Updated: 2024/12/03 10:07:10 by lpittet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@
 # include "libft/includes/libft.h"
 # include <fcntl.h>
 # include <sys/types.h>
+# include <stdio.h>
+# include <errno.h>
 
 #endif
